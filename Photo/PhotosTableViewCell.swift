3573e407b1550c1e2e261acab67e100d31d41750
Dmitrii Kryzhanovskii <dmkryzh@gmail.com>
2021-03-17 17:44:45 +0300

    hw1.1 initial project is uploaded

---
+++ Photo/PhotosTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  PhotosTableViewCell.swift
+//  Navigation
+//
+//  Created by Дмитрий on 24.01.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+    
+    var photosHighlights = PhotosHighlightsView()
+
+    lazy var newConstraints = [
+        photosHighlights.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+        photosHighlights.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+        photosHighlights.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
+        photosHighlights.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12)
+    ]
+    
+//    override init(reuseIdentifier: String?) {
+//        super.init(reuseIdentifier: reuseIdentifier)
+//        contentView.backgroundColor = .white
+//        contentView.addSubview(photosHighlights)
+//        photosHighlights.toAutoLayout()
+//        NSLayoutConstraint.activate(newConstraints)
+//    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .white
+        contentView.addSubview(photosHighlights)
+        photosHighlights.toAutoLayout()
+        NSLayoutConstraint.activate(newConstraints)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+    
